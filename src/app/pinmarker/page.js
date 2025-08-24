3db94fde2094d7cbef0594512dcc569f8d388d6a
Leonardho Rante S <flazen.edu@gmail.com>
2025-08-24 10:31:18 +0700

    [FE] IFrame Pinmarker : Feedback Comparison, Most Visited Pin Category, Most Active User, Bot Distrubution, Pin Visit Distribution, Apps Summary

--- src/app/pinmarker/page.js
+++ src/app/pinmarker/page.js
@@ -1,13 +1,55 @@
+import AtomsIFrame from "@/components/atoms/atoms_iframe";
 import OrganismsFooter from "@/components/organisms/organisms_footer";
 import OrganismsNavbar from "@/components/organisms/organisms_navbar";
 
 const DashboardPinmarkerIndex = () => {    
+    const iframes = [
+        {
+            title: "Feedback Comparison",
+            url: "http://127.0.0.1:8080/EmbedController/feedback_distribution",
+            class: "col-md-4 col-sm-12"
+        },
+        {
+            title: "Most Visited Pin Category",
+            url: "http://127.0.0.1:8080/EmbedController/most_visited_pin_category",
+            class: "col-md-4 col-sm-12"
+        },
+        {
+            title: "Most Active User",
+            url: "http://127.0.0.1:8080/EmbedController/most_active_user",
+            class: "col-md-4 col-sm-12"
+        },
+        {
+            title: "Bot Distrubution",
+            url: "http://127.0.0.1:8080/EmbedController/distribution_bot",
+            class: "col-12"
+        },
+        {
+            title: "Pin Visit Distribution",
+            url: "http://127.0.0.1:8080/EmbedController/distribution_pin_visit",
+            class: "col-12"
+        },
+        {
+            title: "Apps Summary",
+            url: "http://127.0.0.1:8080/EmbedController/apps_summary",
+            class: "col-lg-6 col-md-12"
+        },
+    ];
+
     return (
         <>
             <OrganismsNavbar/>
-                <div className="content">
-
+            <div className="content">
+                <div className="row">
+                    {
+                        iframes.map((item, idx) => (
+                            <div className={item.class}>
+                                <AtomsIFrame key={idx} title={item.title} url={item.url}/>
+                            </div>
+                        ))
+                    }
                 </div>
+            </div>
             <OrganismsFooter/>
         </>
     );
--- src/components/organisms/organisms_footer.js
+++ src/components/organisms/organisms_footer.js
@@ -2,8 +2,8 @@ import AtomsText from "../atoms/atoms_text";
 
 export default function OrganismsFooter() {
     return  (
-        <footer className="footer mt-auto py-3 bg-light">
-            <div className="container-fluid pb-2 py-4 px-4">
+        <footer className="footer mt-auto py-3">
+            <div className="w-100 pb-2 py-4 px-4">
                 <div className="row text-center">
                     <div className="col-lg-4 col-md-4 col-sm-12 pb-2">
                         <AtomsText text_type="mini_sub_heading" body={<span className="text-white">FlazenEye</span>}/>
